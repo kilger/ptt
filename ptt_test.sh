304ba14f2c751265111be4c0ea6ed71464887156
Christopher Kilger <chriskilger+github@gmail.com>
2021-04-01 14:50:20 -0400

    new

---
+++ ptt_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#############################################
+# Check to see if script is running as root #
+#############################################
+#if [ "$EUID" -ne 0 ]
+#  then echo "Please run as root"
+#  exit
+#fi
+
+
+echo "installing bash_aliases from ptt_profile"
+git clone https://github.com/kilger/ptt_profile.git
+cd ptt_profile
+#cat bash_profile >> ~/.bash_aliases
+#source ~/.bash_aliases
+#cd ~/toolz/
+#echo "done".git
